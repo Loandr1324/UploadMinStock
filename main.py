6126d3f6a6199de0f5d7fdf20912a048eb8e66ff
Loik Andrey <7034@balancedv.ru>
2021-11-26 23:18:16 +1100

    Start

---
+++ main.py
@@ -0,0 +1,13 @@
+# Author Loik Andrey 7034@balancedv.ru
+
+
+def Run (file_name):
+    read
+    print(file_name)
+
+
+if __name__ == '__main__':
+    Run('Анализ МО по компании.xlsx')
+    # Run1()
+
+
