3a5b5a3818191bd5eda938bf1825cbc004304e3a
Loik Andrey <7034@balancedv.ru>
2021-11-29 01:17:09 +1100

    1. Добавил функцию по сортировки для удобства восприятия 2. Добавил коментарии 3. Добавил функию для записи и форматирования эксель

--- main.py
+++ main.py
@@ -12,68 +12,42 @@ import os
 
 FOLDER = 'Исходные данные'
 FILE_NAME = 'Анализ МО по компании.xlsx'
+NEW_FILE_NAME = 'Загрузка МО по компании.xlsx'
 
 def Run ():
-    create_df()
+    file_name = FOLDER + "/" + FILE_NAME # Путь к фалйу
+    df_result = create_df(file_name) # Загружаем и подготавливаем DataFrame
+    df_write_xlsx(df_result) # Записываем в эксель
 
-def create_df ():
+def create_df (file):
     """
     :param file_list: Загружаем в DataFrame файлы из file_list
     :param add_name: Добавляем add_name в наименование колонок DataFrame
     :return: df_result Дата фрэйм с данными из файлов
     """
-
-    df_result = None
-
-
-    file = FOLDER + "/" + FILE_NAME
     df = read_excel(file)
-    # TODO Разобраться как считать формулы из эксель
-    df.to_excel('test.xlsx')
-    return
-    df_index = df.index.values.tolist()
-    df.loc['Тип МО'] = 'внешний'
-    df = df.reindex(['Тип МО'] + df_index)
-    df.set_index(['Код', 'Номенклатура'], inplace=True)
-    print (df.loc[(df.index.get_level_values('Код') == 'внешний') & (df.index.get_level_values('Номенклатура') == 'внешний')])
-    print (df['01 Кирова МО'].loc[('внешний', 'внешний')])
 
+    # Добавляем строку со значениями "Внешний" и перемещаем её в начало DataFrame
+    df_index = df.index.values.tolist()
+    df.loc[''] = 'Внешний'
+    df = df.reindex([''] + df_index)
+    df.set_index(['Код', 'Номенклатура'], inplace=True)
+
+    # Копируем колонки и прописываем в них значение "Внутренний" в первую стоку
     for col in df.columns:
         df[col[:-3]] = df[col]
-        df[col[:-3]].loc[('внешний', 'внешний')] = ('внутренний')
-    print (df.loc[('внешний', 'внешний')])
-    # TODO Переименовать колонки (удалить из наименования складов ' МО')
-    # TODO Отсортировать колонки для удобства восприятия
-    df.to_excel('test.xlsx')
-    return
+        df[col[:-3]].loc[('Внешний', 'Внешний')] = ('Внутренний')
 
-    for filename in file_list: # проходим по каждому элементу списка файлов
-        print (filename) # для тестов выводим в консоль наименование файла с которым проходит работа
-        df = read_excel(filename)
-        df_search_header = df.iloc[:15, :2] # для ускорения работы выбираем из DataFrame первую колонку и 15 строк
-        # print (df_search_header)
-        # создаём маску и отмечаем True строку где есть слово "Номенклатура", остальные False
-        mask = df_search_header.replace('.*Номенклатура.*', True, regex=True).eq(True)
-        # Преобразуем Dataframe согласно маски. После обработки все значения будут NaN кроме нужного нам.
-        # В этой же строке кода удаляем все строки со значением NaN и далее получаем индекс оставшейся строки
-        f = df_search_header[mask].dropna(axis=0, how='all').index.values # Удаление пустых колонок, если axis=0, то строк
-        # print (df.iloc[:15, :2])
-        df = df.iloc[int(f):, :] # Убираем все строки с верха DF до заголовков
-        df = df.dropna(axis=1, how='all')  # Убираем пустые колонки
-        df.iloc[0, :] = df.iloc[0, :] + ' ' + add_name # Добавляем в наименование тип данных
-        df.iloc[0, 0] = 'Код'
-        df.iloc[0, 1] = 'Номенклатура'
-        df.columns = df.iloc[0] # Значения из найденной строки переносим в заголовки DataFrame для простоты дальнейшего обращения
-        df = df.iloc[2:, :] # Убираем две строки с верха DF
-        df['Номенклатура'] = df['Номенклатура'].str.strip() # Удалить пробелы с обоих концов строки в ячейке
-        df.set_index(['Код', 'Номенклатура'], inplace=True) # переносим колонки в индекс, для упрощения дальнейшей работы
-        # print(df.iloc[:15, :2]) # Для тестов выводим в консоль 15 строк и два столбца полученного DF
-        # Добавляем преобразованный DF в результирующий DF
-        df_result = concat_df(df_result, df)
-    # Добавляем в результирующий DF по продажам расчётные данные
-    if add_name == 'продажи':
-        df_result = payment(df_result)
-    return df_result
+    # Переименовываем название колонок индексов для корректной записи в эксель
+    df.index.names = ['Номенклатура', '']
+
+    # Сортируем колонки для удобства восприятия
+    df = sort_df(df)
+
+    # Переименовываем колонки (удаляем из наименования складов " МО") для корректной записи в эксель
+    df.columns = df.columns.str.replace(' МО', '')
+
+    return df
 
 def read_excel (file_name):
     """
@@ -81,8 +55,9 @@ def read_excel (file_name):
     :param file_name: Имя файла для чтения
     :return: DataFrame
     """
-    # read_df = pd.read_excel(file_name, header=0, usecols= 'A,B,D,G,J,M,P,S,V,Y', engine='pyxlsb')
-    # read_df = pd.read_excel(file_name, header=0, usecols= 'A,B,D,G,J,M,P,S,V,Y', engine='openpyxl')
+
+    read_df = pd.read_excel(file_name, header=0, usecols='A,B,D,G,J,M,P,S,V,Y', engine='openpyxl')
+
     print ('Попытка загрузки файла:'+file_name)
     try:
         df = read_df
@@ -124,8 +99,139 @@ def bug_fix (file_name):
     shutil.make_archive(f'{FOLDER}/correct_file', 'zip', tmp_folder)
     os.rename(f'{FOLDER}/correct_file.zip', file_name)
 
+def sort_df (df):
+    sort_list = ['01 Кирова', '01 Кирова МО',
+                 '02 Автолюбитель', '02 Автолюбитель МО',
+                 '03 Интер', '03 Интер МО',
+                 '04 Победа', '04 Победа МО',
+                 '08 Центр', '08 Центр МО',
+                 '09 Вокзалка', '09 Вокзалка МО',
+                 '05 Павловский', '05 Павловский МО',
+                 'Компания MaCar', 'Компания MaCar МО']
+    df = df[sort_list]
+    return df
+
+def df_write_xlsx(df):
+    # Сохраняем в переменные значения конечных строк и столбцов
+    row_end, col_end = len(df), len(df.columns)
+    row_end_str, col_end_str = str(row_end), str(col_end)
+
+    # Сбрасываем встроенный формат заголовков pandas
+    pd.io.formats.excel.ExcelFormatter.header_style = None
+
+    # Создаём эксель и сохраняем данные
+    name_file = NEW_FILE_NAME
+    sheet_name = 'Данные'  # Наименование вкладки для сводной таблицы
+    writer = pd.ExcelWriter(name_file, engine='xlsxwriter')
+    workbook = writer.book
+    df.to_excel(writer, sheet_name=sheet_name)
+    wks1 = writer.sheets[sheet_name]  # Сохраняем в переменную вкладку для форматирования
+
+    # Получаем словари форматов для эксель
+    header_format, con_format, border_storage_format_left, border_storage_format_right, \
+    name_format, MO_format, data_format = format_custom(workbook)
+
+    # Форматируем таблицу
+    wks1.set_default_row(12)
+    wks1.set_row(0, 40, header_format)
+    wks1.set_row(1, 10, header_format)
+    wks1.set_column('A:A', 12, name_format)
+    wks1.set_column('B:B', 32, name_format)
+    wks1.set_column('C:R', 10, data_format)
+
+    # Делаем жирным рамку между складами
+    i = 2
+    while i < col_end+2:
+        wks1.set_column(i, i, None, border_storage_format_left)
+        wks1.set_column(i+1, i+1, None, border_storage_format_right)
+        i += 2
+
+    # Объединяем ячейки
+    wks1.merge_range(0, 0, 1, 1, None, None)
+
+    # Добавляем фильтр в первую колонку
+    wks1.autofilter(1, 0, row_end+1, col_end+1)
+    writer.save() # Сохраняем файл
+    return
+
+def format_custom(workbook):
+    header_format = workbook.add_format({
+        'font_name': 'Arial',
+        'font_size': '7',
+        'align': 'center',
+        'valign': 'top',
+        'text_wrap': True,
+        'bold': True,
+        'bg_color': '#F4ECC5',
+        'border': True,
+        'border_color': '#CCC085'
+    })
+
+    border_storage_format_left = workbook.add_format({
+        'num_format': '# ### ##0.00',
+        'font_name': 'Arial',
+        'font_size': '8',
+        'left': 2,
+        'left_color': '#000000',
+        'bottom': True,
+        'bottom_color': '#CCC085',
+        'top': True,
+        'top_color': '#CCC085',
+        'right': True,
+        'right_color': '#CCC085',
+    })
+    border_storage_format_right = workbook.add_format({
+        'num_format': '# ### ##0.00',
+        'font_name': 'Arial',
+        'font_size': '8',
+        'right': 2,
+        'right_color': '#000000',
+        'bottom': True,
+        'bottom_color': '#CCC085',
+        'top': True,
+        'top_color': '#CCC085',
+        'left': True,
+        'left_color': '#CCC085',
+    })
+
+    name_format = workbook.add_format({
+        'font_name': 'Arial',
+        'font_size': '8',
+        'align': 'left',
+        'valign': 'top',
+        'text_wrap': True,
+        'bold': False,
+        'border': True,
+        'border_color': '#CCC085'
+    })
+
+    MO_format = workbook.add_format({
+        'num_format': '# ### ##0.00;;',
+        'bold': True,
+        'font_name': 'Arial',
+        'font_size': '8',
+        'font_color': '#FF0000',
+        # 'text_wrap': True,
+        'border': True,
+        'border_color': '#CCC085'
+    })
+    data_format = workbook.add_format({
+        'num_format': '# ### ##0.00',
+        'font_name': 'Arial',
+        'font_size': '8',
+        'text_wrap': True,
+        'border': True,
+        'border_color': '#CCC085'
+    })
+    con_format = workbook.add_format({
+        'bg_color': '#FED69C',
+    })
+
+    return header_format, con_format, border_storage_format_left, border_storage_format_right, \
+           name_format, MO_format, data_format
+
+
 if __name__ == '__main__':
     Run()
-    # Run1()
 
 
